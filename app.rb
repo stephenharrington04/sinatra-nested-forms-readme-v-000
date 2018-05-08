abe9c4137f1a17b23e167c1b65c6950e8eecfc60
Stephen Harrington <stephenharrington04@yahoo.com>
2018-05-08 18:50:56 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,4 +4,16 @@ class App < Sinatra::Base
   get '/' do
     erb :new
   end
+
+  post 'student' do
+    @student = Student.new(params[:student])
+
+    params[:student][:courses].each do |details|
+      Course.new(details)
+    end
+
+    @courses = Courses.all
+
+    erb :studnet
+  end
 end
