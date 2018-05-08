441681202f005f828b734c57741097d78982ce10
Stephen Harrington <stephenharrington04@yahoo.com>
2018-05-08 18:55:38 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,13 +7,13 @@ class App < Sinatra::Base
 
   post '/student' do
     @student = Student.new(params[:student])
-
+ 
     params[:student][:courses].each do |details|
       Course.new(details)
     end
-
-    @courses = Courses.all
-
+ 
+    @courses = Course.all
+ 
     erb :student
   end
 end
